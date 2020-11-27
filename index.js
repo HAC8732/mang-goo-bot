3c0bdafbcf1550f73946d6f3a27ebb49a9b4fbcf
HAC8732 <cwu5985@naver.com>
2020-11-27 12:51:34 +0900

    bot upload

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const token = process.env.token;
 const welcomeChannelName = "환영한다-동무들";
-const byeChannelName = "동무 반갑소";
+const byeChannelName = "잘가시고";
 const welcomeChannelComment = "동무 반갑소";
 const byeChannelComment = "잘가시고";
 
